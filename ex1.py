f9de78b45354c87249b4aff60adf2c065497ce0f
sophia-h-le <61862324+sophia-h-le@users.noreply.github.com>
2021-07-08 16:41:42 -0700

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -5,3 +5,6 @@ print("This is fun.")
 print("Yay! Printing.")
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
+
+# Drill
+# Octothorpe symbol: the compiler will ignores everything after it on that line while compiling. 
