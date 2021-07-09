79e7374ed056827a9c4ba9637f4699baccb17548
sophia-h-le <61862324+sophia-h-le@users.noreply.github.com>
2021-07-08 22:19:25 -0700

    Update and rename ex6.py to ex006.py

--- ex006.py
+++ ex006.py
@@ -3,12 +3,15 @@ x = f"There are {types_of_people} types of people."
 
 binary = "binary"
 do_not = "don't"
+# 2 string variables in a string
 y = f"Those who know {binary} and those who {do_not}."
 
 print(x)
 print(y)
 
+# String variable in a string
 print(f"I said: {x}")
+# String variable in a string
 print(f"I also said: '{y}'")
 
 hilarious = False
@@ -19,4 +22,9 @@ print(joke_evaluation.format(hilarious))
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# 2 string variables in a string
 print(w + e)
+
+# Drill
+
+# When concatenate with the '+' sign, the operation will fail if the operand follows the plus sign is not a string even if if implements the __str__ method
