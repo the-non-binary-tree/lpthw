41b2ad10dace01d793ff6daf8500763973a2c785
sophia-h-le <61862324+sophia-h-le@users.noreply.github.com>
2021-07-08 22:04:37 -0700

    Update and rename ex4.py to ex004.py

--- ex004.py
+++ ex004.py
@@ -13,3 +13,12 @@ print("There will be", cars_not_driven, "empty cars today.")
 print("We can transport", carpool_capacity, "people today.")
 print("We have", passengers, "to carpool today.")
 print("We need to pull about", average_passengers_per_car, "in each car.")
+
+# Drill
+# “Traceback (most recent call last):
+#   File "ex4.py", line 8, in <module>
+#     average_passengers_per_car = car_pool_capacity / passenger
+# NameError: name 'car_pool_capacity' is not defined”
+# There was no variable named 'car_pool_capacity'
+
+# 4.0 is float, 4 is integer. If you need decimal value then float is more suitable. 
