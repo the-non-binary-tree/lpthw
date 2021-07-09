476a9abcdb062a6f78c9a2823a9e3a84e01d3e54
sophia-h-le <61862324+sophia-h-le@users.noreply.github.com>
2021-07-08 22:22:45 -0700

    Update and rename ex7.py to ex007.py

--- ex007.py
+++ ex007.py
@@ -19,3 +19,11 @@ end12 = "r"
 #watch that comma at the end. try removing it to see what happends
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
+
+# Drill 
+
+# Removing the comma resulted in 
+# File "ex007.py", line 20
+#     print(end1 + end2 + end3 + end4 + end5 + end6 end=' ')
+#                                                     ^
+# SyntaxError: invalid syntax
