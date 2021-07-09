1ee307ff334aa0dc114e1e8d4c8f6740b2faa06a
sophia-h-le <61862324+sophia-h-le@users.noreply.github.com>
2021-07-08 21:56:19 -0700

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,27 +1,28 @@
 print("I will now count my chickens:")
 
 # Addition of 25 and (30 divided by 6)
-print("Hens", float(25) + float(30) / float(6))
+print("Hens", 25.0 + 30.0 / 6.0)
 # Subtraction from 100 the value of (modulo of (25 multipied by 3) divided by 4)
-print("Rooster", float(100) - float(25) * float(3) % float(4))
+print("Rooster", 100.0 - 25.0 * 3.0 % 4.0)
 
 print("Now I will count the eggs:")
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
-print("Is it true that 3 + 2 < 5 - 7?")
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
-print(3 + 2 < 5 - 7)
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7", 5 - 7)
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3.0 + 2.0?", 3.0 + 2.0)
+print("What is 5.0 - 7.0", 5.0 - 7.0)
 
 print("Oh, that's why it's False.")
 
 print("How about some more.")
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
 
 # Drill
