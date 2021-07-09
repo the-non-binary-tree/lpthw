6ba84ad6022f0a143edb0de5c485a985aa45ea91
sophia-h-le <61862324+sophia-h-le@users.noreply.github.com>
2021-07-08 22:31:18 -0700

    Update ex008.py

--- ex008.py
+++ ex008.py
@@ -4,12 +4,12 @@ print(formatter.format(1, 2, 3, 4))
 print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
 print(formatter.format(formatter, formatter, formatter, formatter))
- 7    print(formatter.format(
- 8        "Try your",
- 9        "Own text here",
-10        "Maybe a poem",
-11        "Or a song about fear"
-12    ))
+print(formatter.format(
+  "Try your",
+  "Own text here",
+  "Maybe a poem",
+  "Or a song about fear"
+))
 
 #   Drill
 
