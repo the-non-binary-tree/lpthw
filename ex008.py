06b753d19a77974f7443a14b0c039981b5543c8e
sophia-h-le <61862324+sophia-h-le@users.noreply.github.com>
2021-07-08 22:28:41 -0700

    Update and rename ex8.py to ex008.py
    An interesting way to turn a variable into another string

---
+++ ex008.py
@@ -0,0 +1,16 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+ 7    print(formatter.format(
+ 8        "Try your",
+ 9        "Own text here",
+10        "Maybe a poem",
+11        "Or a song about fear"
+12    ))
+
+#   Drill
+
+
