ed09b850fa6b0ef45bc249431b40b6f0c078e899
Sophia Le <61862324+sophia-h-le@users.noreply.github.com>
2021-07-30 13:10:34 -0700

    Update ex015.py

--- ex015.py
+++ ex015.py
@@ -6,11 +6,13 @@ txt = open(filename)
 
 print(f"Here's your file {filename}:")
 print(txt.read())
+txt.close()
 
-print("Type the filename again:")
-file_again = input(">")
+# print("Type the filename again:")
+# file_again = input(">")
 
-txt_again = open(file_again)
+# txt_again = open(file_again)
 
-print(txt_again.read())
+# print(txt_again.read())
+# txt_again.close()
 
