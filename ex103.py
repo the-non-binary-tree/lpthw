23d532626557f5d3b5f0a877d54c6b1a549d7aa7
Sophia Le <61862324+sophia-h-le@users.noreply.github.com>
2021-07-30 13:03:16 -0700

    Update and rename ex13.py to ex103.py

--- ex103.py
+++ ex103.py
@@ -6,3 +6,6 @@ print("the script is called:", script)
 print("Your first variable is", first)
 print("Your second variable is", second)
 print("Your third variable is", third)
+
+fourth = input("Give one more variable: ")
+print("Your fourth variable is", fourth)
