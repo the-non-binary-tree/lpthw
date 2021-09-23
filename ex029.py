ded897e9d4cbc2da8f02d80ed83adc2abca41f9e
The Non-binary Tree <61862324+the-non-binary-tree@users.noreply.github.com>
2021-09-23 11:00:55 -0700

    Update ex029.py

--- ex029.py
+++ ex029.py
@@ -25,3 +25,20 @@ if people <= dogs:
   
 if people == dogs:
   print("People are dogs.")
+
+#Drill
+#What if does to the code under it:
+# # The IF statement is a decision-making statement 
+# # that guides a program to make decisions based on 
+# # specified criteria. The IF statement executes 
+# # one set of code if a specified condition is met (TRUE) 
+# # or another set of code evaluates to FALSE.
+#Why the code under if needs to be indented four spaces:
+# # in Python, it is required for indicating what 
+# # block of code a statement belongs to
+#What happens if it isn't indented:
+# # Depends on the actual code, but basically it will 
+# # be executed regardless of the value of the expressions
+# # following the if statements
+#What happens if change the initial values of variables:
+# # The expressions in the if statements will validate to different values
